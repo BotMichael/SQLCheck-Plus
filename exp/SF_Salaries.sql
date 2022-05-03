019fcca2f11a0c37fa782a759261d79be2cf2318
wws <351483565@qq.com>
2022-05-03 00:21:05 -0400

    added sample sqlite file

---
+++ exp/SF_Salaries.sql
@@ -0,0 +1,14 @@
+CREATE TABLE Salaries (
+    Id INTEGER PRIMARY KEY,
+    EmployeeName TEXT,
+    JobTitle TEXT,
+    BasePay NUMERIC,
+    OvertimePay NUMERIC,
+    OtherPay NUMERIC,
+    Benefits NUMERIC,
+    TotalPay NUMERIC,
+    TotalPayBenefits NUMERIC,
+    Year INTEGER,
+    Notes TEXT,
+    Agency TEXT,
+    Status TEXT);
